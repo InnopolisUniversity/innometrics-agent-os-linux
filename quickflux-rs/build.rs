487aac13cb8f93d73193265d94fe5b309676c39b
ivan tkachenko <me@ratijas.tk>
2020-04-29 16:27:33 +0300

    fix quickflux-rs build process

--- quickflux-rs/build.rs
+++ quickflux-rs/build.rs
@@ -1,4 +1,5 @@
 use std::process::Command;
+use std::env;
 
 /// This function was copied from qmetaobject-rs build script.
 fn qmake_query(var: &str) -> String {
@@ -11,31 +12,63 @@ fn qmake_query(var: &str) -> String {
             .expect("Failed to execute qmake. Make sure 'qmake' is in your path")
             .stdout,
     )
-    .expect("UTF-8 conversion failed")
+        .expect("UTF-8 conversion failed")
+}
+
+/// Cargo build profile, value of `$PROFILE` env variable.
+enum Profile {
+    /// Default mode
+    Debug,
+    /// `--release` mode.
+    Release,
+}
+
+impl Profile {
+    pub fn get() -> Self {
+        match env::var("PROFILE").expect("Cargo profile is not set").as_str() {
+            "debug" => Profile::Debug,
+            "release" => Profile::Release,
+            x => panic!("unknown cargo profile: {}", x),
+        }
+    }
 }
 
 fn main() {
+    /*
+     * Qt links
+     */
+
     // parts of this code were also copied from qmetaobject-rs
     let qt_include_path = qmake_query("QT_INSTALL_HEADERS");
     let qt_library_path = qmake_query("QT_INSTALL_LIBS");
-
-    let dst = cmake::Config::new("vendor/quickflux")
-        .build();
-
-    let mut quickflux_include_path = dst.clone();
-    quickflux_include_path.push("include");
-    quickflux_include_path.push("quickflux");
 
     println!("cargo:rustc-link-search={}", qt_library_path.trim());
     println!("cargo:rustc-link-lib=Qt5Core");
     println!("cargo:rustc-link-lib=Qt5Quick");
     println!("cargo:rustc-link-lib=Qt5Qml");
 
+    /*
+     * QuickFlux links
+     */
+
+    let dst = cmake::Config::new("vendor/quickflux").build();
+    let quickflux = match Profile::get() {
+        Profile::Debug => "quickfluxd",
+        Profile::Release => "quickflux",
+    };
+
+    println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
+    println!("cargo:rustc-link-lib=static={}", quickflux);
+
+    /*
+     * cpp! macro
+     */
+
     cpp_build::Config::new()
         .include(qt_include_path.trim())
         .include(format!("{}/QtCore", qt_include_path.trim()))
         .include(format!("{}/QtQuick", qt_include_path.trim()))
         .include(format!("{}/QtQml", qt_include_path.trim()))
-        .include(quickflux_include_path)
+        .include(dst.join("include/quickflux"))
         .build("src/lib.rs");
 }
--- quickflux-rs/src/lib.rs
+++ quickflux-rs/src/lib.rs
@@ -1,13 +1,11 @@
 #[macro_use]
 extern crate cpp;
 
-cpp!{{
-
-#include <QuickFlux>
-
+cpp! {{
+    #include <QuickFlux>
 }}
 
-fn register_quickflux_qml_types() {
+pub fn register_quickflux_qml_types() {
     cpp!(unsafe [] {
         registerQuickFluxQmlTypes();
     });
