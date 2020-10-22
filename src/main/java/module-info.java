1c555d1a10b0b6534df698d669063782d12e5a01
Gci04 <gcinzoe04@gmail.com>
2020-10-22 17:17:37 +0300

    Add FAQ

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,4 +11,9 @@ open module innometrics.agent.os.linux.main {
     requires json;
     requires java.sql;
     requires sqlite.jdbc;
+<<<<<<< HEAD
+=======
+    requires SystemTray;
+    requires jnativehook;
+>>>>>>> 62b25d2... Add FAQ
 }
