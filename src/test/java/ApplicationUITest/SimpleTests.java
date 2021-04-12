c10b6467d2ad9ed7864e795da972c00ba44e4eb2
Gci04 <gcinzoe04@gmail.com>
2021-04-12 17:11:26 +0300

    Init GPU-2

---
+++ src/test/java/ApplicationUITest/SimpleTests.java
@@ -0,0 +1,21 @@
+package ApplicationUITest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTests {
+    @Test
+    public void test1(){
+        Assertions.assertTrue(true);
+    }
+    @Test
+    public void test2(){
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void test3(){
+        Assertions.assertTrue(true);
+    }
+
+}
